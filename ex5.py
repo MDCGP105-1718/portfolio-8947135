2b3242466ad9c47089f51e7d9961cace48aa883e
Daniel Pokladek <danielpokladek@gmail.com>
2017-10-05 10:48:42 +0100

    first commit of exercise 5

---
+++ ex5.py
@@ -0,0 +1,4 @@
+for n in range(1,99,1):
+    if(n<=99):
+        print("99 bottles of beer on the wall, 99 bottles of beer.")
+        print("Take on down, pass it around, 98 bottles of beer on the wall...")
