4cf4901869246336d60ea78d6e7b78e6f89dad1f
Daniel Pokladek <danielpokladek@gmail.com>
2017-10-05 10:44:55 +0100

    first commit of exercise 6

---
+++ ex6.py
@@ -0,0 +1,8 @@
+name = input('Name :')
+age  = input('Age :')
+height = input('Height(in cm) :')
+eyecolour = input ('Eye colour :')
+haircolour = input ('Hair colour :')
+
+print(f"Say your name {name}")
+print(f"Say your age {age}")
